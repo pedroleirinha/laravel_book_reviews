ef40024740fefdfa8537b64317f2634a0ece4980
Pedro Oleirinha <oleirinha.pedro@gmail.com>
2025-07-13 21:21:26 +0100

    created a book crontroller and view into which it will feed the books to display. Also a search feature with a search bar and clear button

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,40 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopeMinResults(Builder $query, int $numResults): Builder
+    {
+        return $query->having('reviews_count', '>=', $numResults);
+    }
+
+    private function dateRangeFilter(Builder $q, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $q->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $q->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $q->whereBetween('created_at', [$from, $to]);
+        }
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query
+            ->withCount(['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query
+            ->withAvg(['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
     }
 }
